dc361507f2400b63349ceaf30eb48beb2aa71bda
Manuel Baez <utp0009646@alumno.utpuebla.edu.mx>
2024-07-15 18:47:13 -0600

    Inicialización del proyecto

---
+++ src/main/java/com/projectone/spliteralura/SpliteraluraApplication.java
@@ -0,0 +1,13 @@
+package com.projectone.spliteralura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpliteraluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpliteraluraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projectone/spliteralura/SpliteraluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectone.spliteralura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpliteraluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
